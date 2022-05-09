4ce31ffd8d9710cc4c0c7581d84473fb69697879
Lautaro Lazzeri <lautyylazzerii@gmail.com>
2022-05-09 19:53:39 -0300

    update

---
+++ peliculas/lib/providers/movies_provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:peliculas/models/models.dart';
+
+
+class MoviesProvider extends ChangeNotifier {
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _apiKey = '34ebbdacdbd2cec16d9df6c3080acd07';
+  final String _lenguaje = 'es-ES';
+
+
+  MoviesProvider() {
+    print('MoviesProvider inicializando');
+
+    getOnDisplayMovies();
+  }
+
+  getOnDisplayMovies() async {
+    var url = Uri.https(_baseUrl, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _lenguaje, 'page': '1'});
+
+    final response = await http.get(url);
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+    print(nowPlayingResponse.results[1].title);
+  }
+}
